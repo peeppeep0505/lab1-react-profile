8d8bc9e882b503be5de98162f23f2a9e0542452d
peeppeep0505 <sirikorn.jaisri@gmail.com>
2026-01-05 15:47:51 +0700

    add searchterm,delete skill

--- my-profile/src/components/ProfileCard.jsx
+++ my-profile/src/components/ProfileCard.jsx
@@ -3,15 +3,26 @@ import { useState } from 'react';
 function ProfileCard({ name, role, bio }) {
     const [likes, setLikes] = useState (0);
     
-      const [skills, setSkills] = useState (["React", "JavaScript"]);
-      const [newSkill, setNewSkill] = useState ("");
+    const [skills, setSkills] = useState (["React", "JavaScript"]);
+    const [newSkill, setNewSkill] = useState ("");
 
-      const addSkill = () => {
+    const addSkill = () => {
         if (newSkill.trim() !== "") {
         setSkills([...skills, newSkill]);
         setNewSkill("");
         }
     };
+
+    const [searchTerm, setSearchTerm] = useState ("");
+
+    const filterSkills = skills.filter(skill => 
+        skill.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const deleteSkill = (indexToDelete) => {
+    const updatedSkills = skills.filter((_, index) => index !== indexToDelete);
+    setSkills (updatedSkills);
+  }; 
   return (
     <div
       style={{
@@ -27,20 +38,28 @@ function ProfileCard({ name, role, bio }) {
       <p>{bio}</p>
 
        <div>
+        <h3>Skills</h3>
         <input
-          style={{ margin: '5px 0' }}
-          value={newSkill}
-          onChange={(e) => setNewSkill(e.target.value)}
+          placeholder="Search skills..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {filterSkills.map((skill, index) => (
+              <li key={index} style={{ color: skill.toLowerCase().includes("react") ? 'blue' : 'black' }} >
+                {skill}
+                <button onClick={() => deleteSkill(index)} style={{ marginLeft: '10px' }}>Delete</button>
+                </li>
+            ))}
+        
+          <input
+          placeholder="add skills..."
+            value={newSkill}
+            onChange={(e) => setNewSkill(e.target.value)}
           />
         <button onClick={addSkill} style={{ margin: '0 2px' }}>Add Skill</button>
-          <ul style={{ textAlign: 'left' }}>
-            {skills.map((skill, index) => (
-              <li key={index}>{skill}</li>
-            ))}
-          </ul>
         </div>
 
-      <button onClick={() => setLikes(likes + 1)}>
+      <button onClick={() => setLikes(likes + 1)} style={{ margin: '10px 0' }}>
         🩷 Like {likes}
       </button>
     </div>
